f4d39d1dcf42617803871cdb7635b5184387c615
atyenoria <eigotyoubunn30@gmail.com>
2017-03-27 08:57:20 +0900

    queue

--- src/App.js
+++ src/App.js
@@ -2,11 +2,60 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var Queue = function () {
+    var queue = Promise.resolve(true);
+    var add = function (job) {
+      queue = queue.then(job);
+    };
+    this.add = add;
+};
+
+var queue = new Queue();
+
 class App extends Component {
 
   componentDidMount=()=>{
-    console.log("test")
+    queue.add(function () {
+      var promise = new Promise(function (resolve) {
+        setTimeout(function () {
+          console.log('1');
+          resolve();
+        }, 100);
+      });
+      return promise;
+    });
+    queue.add(function () {
+      var promise = new Promise(function (resolve) {
+        setTimeout(function () {
+          console.log('2');
+          resolve();
+        }, 200);
+      });
+      return promise;
+    });
+    queue.add(function () {
+      var promise = new Promise(function (resolve) {
+        setTimeout(function () {
+          console.log('3');
+          resolve();
+        }, 300);
+      });
+      return promise;
+    });
   }
+
+  test1=()=>{
+    queue.add(function () {
+      var promise = new Promise(function (resolve) {
+        setTimeout(function () {
+          console.log('1');
+          resolve();
+        }, 100);
+      });
+      return promise;
+    });
+  }
+
 
   render() {
     return (
@@ -18,6 +67,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={()=> {this.test1()}} >{"test1"}</button>
       </div>
     );
   }
