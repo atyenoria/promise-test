4db16b5e50c547fe7636ccc449d273027212dd6a
atyenoria <eigotyoubunn30@gmail.com>
2017-03-27 14:05:18 +0900

    p-queue example

--- src/App.js
+++ src/App.js
@@ -1,59 +1,82 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const PQueue = require('p-queue');
+const got = require('got');
+const queue = new PQueue({concurrency: 1});
 
-var Queue = function () {
-    var queue = Promise.resolve(true);
-    var add = function (job) {
-      queue = queue.then(job);
-    };
-    this.add = add;
-};
+// var Queue = function () {
+//     var queue = Promise.resolve(true);
+//     var add = function (job) {
+//       queue = queue.then(job);
+//     };
+//     this.add = add;
+// };
 
-var queue = new Queue();
+// var queue = new Queue();
 
 class App extends Component {
 
   componentDidMount=()=>{
-    queue.add(function () {
-      var promise = new Promise(function (resolve) {
-        setTimeout(function () {
-          console.log('1');
-          resolve();
-        }, 100);
-      });
-      return promise;
+    queue.onEmpty().then(() => {
+      console.log('7. Queue is empty');
     });
-    queue.add(function () {
-      var promise = new Promise(function (resolve) {
-        setTimeout(function () {
-          console.log('2');
-          resolve();
-        }, 200);
-      });
-      return promise;
+
+    queue.add(() => got('https://sindresorhus.com')).then(() => {
+      console.log('Done: sindresorhus.com');
     });
-    queue.add(function () {
-      var promise = new Promise(function (resolve) {
-        setTimeout(function () {
-          console.log('3');
-          resolve();
-        }, 300);
-      });
-      return promise;
+    queue.add(() => got('https://sindresorhus.com')).then(() => {
+      console.log('Done: sindresorhus.com');
     });
+    queue.add(() => got('https://sindresorhus.com')).then(() => {
+      console.log('Done: sindresorhus.com');
+    });
+    queue.add(() => got('https://sindresorhus.com')).then(() => {
+      console.log('Done: sindresorhus.com');
+    });
+
+    queue.add(() => Promise.resolve('🦄')).then(console.log.bind(null, '5. Resolved'));
+    queue.add(() => Promise.resolve('🐴')).then(console.log.bind(null, '6. Resolved'));
+
+    // queue.add(function () {
+    //   var promise = new Promise(function (resolve) {
+    //     setTimeout(function () {
+    //       console.log('1');
+    //       resolve();
+    //     }, 100);
+    //   });
+    //   return promise;
+    // });
+    // queue.add(function () {
+    //   var promise = new Promise(function (resolve) {
+    //     setTimeout(function () {
+    //       console.log('2');
+    //       resolve();
+    //     }, 200);
+    //   });
+    //   return promise;
+    // });
+    // queue.add(function () {
+    //   var promise = new Promise(function (resolve) {
+    //     setTimeout(function () {
+    //       console.log('3');
+    //       resolve();
+    //     }, 300);
+    //   });
+    //   return promise;
+    // });
+
+    // for (;;) {
+    //   console.log(queue)
+    // }
   }
 
   test1=()=>{
-    queue.add(function () {
-      var promise = new Promise(function (resolve) {
-        setTimeout(function () {
-          console.log('1');
-          resolve();
-        }, 100);
-      });
-      return promise;
+    queue.onEmpty().then(() => {
+      console.log('7. Queue is empty');
     });
+    queue.add(() => Promise.resolve('🦄')).then(console.log.bind(null, '5. Resolved'));
+    queue.add(() => Promise.resolve('🐴')).then(console.log.bind(null, '6. Resolved'));
   }
 
 
